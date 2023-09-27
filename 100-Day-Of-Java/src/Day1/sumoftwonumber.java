c843ebdb56c417e93ceeab9546eb7b6dc9d4fcd4
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2023-09-27 14:48:40 +0000

    my day 1 code basics

---
+++ 100-Day-Of-Java/src/Day1/sumoftwonumber.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class sumoftwonumber{
+    public static void main(String[] args){
+        int a =10;
+        int b =20;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+
+}
---
+++ 100-Day-Of-Java/src/Day2/product.java
@@ -0,0 +1,10 @@
+package Day2;
+
+public class product {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+        int product = a*b;
+        System.out.println(product);
+    }
+}
