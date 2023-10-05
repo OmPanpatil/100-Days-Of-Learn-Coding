5feb06d055f6e761629f137f29ba94d432a63835
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2023-10-05 17:13:37 +0000

    KickStart

---
+++ 100-Day-Of-Java/src/Day10/functions1.java
@@ -0,0 +1,12 @@
+
+public class functions1{
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        //body
+
+        System.out.println("Hello, world");
+
+    }
+}
---
+++ 100-Day-Of-Java/src/Day10/functions2.java
@@ -0,0 +1,10 @@
+public class functions2 {
+    public static void printHelloWorld(){
+        System.out.println("Hello,world");
+        System.out.println("Hello, world");
+    }
+
+    public static void main(String[] args){
+        printHelloWorld();
+    }
+}
