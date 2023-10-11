3301a197c2af9d9cfedb5f98472408f072e4f8df
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2023-10-11 17:14:26 +0000

    mastering the pattern best part 2

---
+++ 100-Day-Of-Java/src/Day16/patternp203.java
@@ -0,0 +1,21 @@
+public class patternp203{
+    public static void inverted_half_pyramid_with_numbers(int n){
+        // outer loop
+
+        for(int i=1; i<= n; i++){
+            // inner loop
+
+            for(int j=1; j<= n-i+1; j++){
+                System.out.print(j + " ");
+
+            }
+
+            System.out.println();
+
+        }
+    }
+
+    public static void main(String[] args){
+        inverted_half_pyramid_with_numbers(5);
+    }
+}
